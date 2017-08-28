82d15db5b25537af6c30bb92811ac7461974bf02
Wisn <wuyishun_kmk@outlook.com>
2017-08-28 20:38:24 +0800

    add test

--- NativeTest/app/src/main/java/want/com/nativetest/MainActivity.java
+++ NativeTest/app/src/main/java/want/com/nativetest/MainActivity.java
@@ -3,7 +3,6 @@ package want.com.nativetest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
