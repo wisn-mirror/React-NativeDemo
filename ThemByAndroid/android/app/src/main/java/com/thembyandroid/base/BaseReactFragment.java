158090e526f226199776adf2fcdd8bc25f8ad233
Wisn <wuyishun_kmk@outlook.com>
2017-09-21 10:29:54 +0800

    fix fragment TouchEvent bug

--- ThemByAndroid/android/app/src/main/java/com/thembyandroid/base/BaseReactFragment.java
+++ ThemByAndroid/android/app/src/main/java/com/thembyandroid/base/BaseReactFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -15,48 +14,24 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
-import com.thembyandroid.them.MTouchListener;
-import com.thembyandroid.them.RadioButtonViewPagerNavigatorActivity;
-import com.wisn.skinlib.utils.LogUtils;
 
 /**
  * Created by wisn on 2017/9/18.
  */
 
-public abstract class BaseReactFragment extends Fragment implements MTouchListener {
+public abstract class BaseReactFragment extends Fragment  {
     private ReactRootView mReactRootView;
     private ReactInstanceManager mReactInstanceManager;
-    private boolean isload=false;
-    private LinearLayout mInflate;
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-//        mInflate = (LinearLayout)View.inflate(context, R.layout.fragment_root, null);
-
-//        mReactInstanceManager.addReactInstanceEventListener();
-        ((RadioButtonViewPagerNavigatorActivity) getActivity()).setMTouchListener(this);
-        /*mReactInstanceManager = ReactInstanceManager.builder()
-                                                    .setApplication(getActivity().getApplication())
-                                                    *//*.setBundleAssetName("index.android.bundle")
-                                                    .setJSMainModuleName("index.android")*//*
-                                                    .setNativeModuleCallExceptionHandler(new NativeModuleCallExceptionHandler() {
-                                                        @Override
-                                                        public void handleException(Exception e) {
-                                                            e.printStackTrace();
-                                                        }
-                                                    })
-                                                    .addPackage((ReactPackage) getActivity())
-                                                    .setUseDeveloperSupport(false)
-                                                    .setInitialLifecycleState(LifecycleState.RESUMED)
-                                                    .build();*/
         mReactRootView = new ReactRootView(context);
         mReactInstanceManager =
                 ((BaseReactApplication) getActivity().getApplication()).getReactNativeHost()
                                                                        .getReactInstanceManager();
+        mReactRootView.startReactApplication(mReactInstanceManager,getMainPageName(),null);
 
-
-//        mInflate.addView(mReactRootView);
     }
 
     @Override
@@ -73,7 +48,6 @@ public abstract class BaseReactFragment extends Fragment implements MTouchListen
                              @Nullable Bundle savedInstanceState) {
          super.onCreateView(inflater, container, savedInstanceState);
         return mReactRootView;
-//        return mInflate;
     }
 
     @Override
@@ -85,21 +59,16 @@ public abstract class BaseReactFragment extends Fragment implements MTouchListen
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        if(!isload){
-            mReactRootView.startReactApplication(mReactInstanceManager,getMainPageName(),null);
-            isload=true;
-        }
-
     }
 
 
     @Override
     public void onDestroyView() {
-        mReactRootView.unmountReactApplication();
-        super.onDestroyView();
-        /*if (mReactRootView != null) {
+        if (mReactRootView != null) {
             ((ViewGroup) mReactRootView.getParent()).removeView(mReactRootView);
-        }*/
+        }
+        super.onDestroyView();
+
     }
 
     @Override
@@ -119,14 +88,6 @@ public abstract class BaseReactFragment extends Fragment implements MTouchListen
             ((BaseReactApplication) getActivity().getApplication()).getReactContext()
                                                             .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
                                                             .emit(eventName, params);
-        }
-    }
-    public void dispatchTouchEvent(MotionEvent event){
-        if(mReactRootView!=null){
-            LogUtils.e("dispatchTouchEvent","getAction:"+event.getAction());
-//            mReactRootView.onTouchEvent(event);
-//            mReactRootView.onChildStartedNativeGesture(event);
-//            mReactRootView.onChildStartedNativeGesture(event);
         }
     }
 }
--- ThemByAndroid/android/app/src/main/java/com/thembyandroid/them/RadioButtonViewPagerNavigatorActivity.java
+++ ThemByAndroid/android/app/src/main/java/com/thembyandroid/them/RadioButtonViewPagerNavigatorActivity.java
@@ -4,19 +4,24 @@ import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
-import android.view.MotionEvent;
 import android.widget.RadioGroup;
 
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
 import com.thembyandroid.R;
 import com.thembyandroid.adapter.FragmentAdapter;
+import com.thembyandroid.base.BaseReactActivity;
+import com.thembyandroid.base.BaseReactApplication;
 import com.thembyandroid.view.MyRadioButton;
-import com.wisn.skinlib.base.SkinFragmentActivity;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class RadioButtonViewPagerNavigatorActivity extends SkinFragmentActivity implements RadioGroup.OnCheckedChangeListener,
-                                                                                            ViewPager.OnPageChangeListener {
+public class RadioButtonViewPagerNavigatorActivity extends BaseReactActivity implements RadioGroup.OnCheckedChangeListener,
+                                                                                        ViewPager.OnPageChangeListener {
 
     private RadioGroup mRadioButton;
     private MyRadioButton mRadiobutton_bg_home;
@@ -24,19 +29,26 @@ public class RadioButtonViewPagerNavigatorActivity extends SkinFragmentActivity 
     private MyRadioButton mRadiobutton_bg_start;
     private MyRadioButton mRadiobutton_bg_watch;
     private ViewPager mViewpager;
+    private ReactInstanceManager mReactInstanceManager;
 
-    private List<String> data=new ArrayList<String>();
+    private List<String> data = new ArrayList<String>();
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigator_viewpager);
-        mRadioButton = (RadioGroup)findViewById(R.id.bottom_radiogroup);
-        mViewpager = (ViewPager)findViewById(R.id.viewpager);
-        mRadiobutton_bg_home = (MyRadioButton)findViewById(R.id.radiobutton_bg_home);
-        mRadiobutton_bg_gift = (MyRadioButton)findViewById(R.id.radiobutton_bg_gift);
-        mRadiobutton_bg_start = (MyRadioButton)findViewById(R.id.radiobutton_bg_start);
-        mRadiobutton_bg_watch = (MyRadioButton)findViewById(R.id.radiobutton_bg_watch);
+        mRadioButton = (RadioGroup) findViewById(R.id.bottom_radiogroup);
+        mViewpager = (ViewPager) findViewById(R.id.viewpager);
+        mRadiobutton_bg_home = (MyRadioButton) findViewById(R.id.radiobutton_bg_home);
+        mRadiobutton_bg_gift = (MyRadioButton) findViewById(R.id.radiobutton_bg_gift);
+        mRadiobutton_bg_start = (MyRadioButton) findViewById(R.id.radiobutton_bg_start);
+        mRadiobutton_bg_watch = (MyRadioButton) findViewById(R.id.radiobutton_bg_watch);
         mRadioButton.setOnCheckedChangeListener(this);
+        mReactInstanceManager =
+                ((BaseReactApplication) this.getApplication()).getReactNativeHost()
+                                                              .getReactInstanceManager();
+
+
     }
 
     @Override
@@ -45,14 +57,28 @@ public class RadioButtonViewPagerNavigatorActivity extends SkinFragmentActivity 
         setDefaultFragment();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostPause(this);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostResume(this, this);
+        }
+    }
+
     private void setDefaultFragment() {
-//        mRadiobutton_bg_home.setChecked(true);
-//        mRadiobutton_bg_gift.setChecked(true);
         data.add("HomeFragment");
         data.add("GiftFragment");
         data.add("StartFragment");
         data.add("WatchFragment");
-        FragmentAdapter fragmentAdapter=new FragmentAdapter(getSupportFragmentManager(), data);
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), data);
         mViewpager.setAdapter(fragmentAdapter);
         mViewpager.addOnPageChangeListener(this);
         mViewpager.setCurrentItem(1);
@@ -60,19 +86,19 @@ public class RadioButtonViewPagerNavigatorActivity extends SkinFragmentActivity 
 
     @Override
     public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
-        int  index=0;
-        switch (checkedId){
+        int index = 0;
+        switch (checkedId) {
             case R.id.radiobutton_bg_home:
-                index=0;
+                index = 0;
                 break;
             case R.id.radiobutton_bg_gift:
-                index=1;
+                index = 1;
                 break;
             case R.id.radiobutton_bg_start:
-                index=2;
+                index = 2;
                 break;
             case R.id.radiobutton_bg_watch:
-                index=3;
+                index = 3;
                 break;
         }
         mViewpager.setCurrentItem(index);
@@ -86,7 +112,7 @@ public class RadioButtonViewPagerNavigatorActivity extends SkinFragmentActivity 
     @Override
     public void onPageSelected(int position) {
         mRadioButton.setOnCheckedChangeListener(null);
-        switch (position){
+        switch (position) {
             case 0:
                 mRadiobutton_bg_home.setChecked(true);
                 break;
@@ -107,44 +133,18 @@ public class RadioButtonViewPagerNavigatorActivity extends SkinFragmentActivity 
     public void onPageScrollStateChanged(int state) {
 
     }
-//
-//    @Override
-//    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-//        ArrayList nativeModuleList = new ArrayList<>();
-//        nativeModuleList.add(new MainModule(reactContext));
-//        return nativeModuleList;
-//    }
-//
-//    @Override
-//    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-//        return Collections.emptyList();
-//    }
 
     @Override
-    public boolean dispatchTouchEvent(MotionEvent event) {
-        int currentItem = mViewpager.getCurrentItem();
-        if(currentItem==1){
-            if(mMTouchListener!=null){
-                mMTouchListener.dispatchTouchEvent(event);
-            }
-        }
-        return super.dispatchTouchEvent(event);
-//        return true;
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        ArrayList nativeModuleList = new ArrayList<>();
+        nativeModuleList.add(new MainModule(reactContext));
+        return nativeModuleList;
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        int currentItem = mViewpager.getCurrentItem();
-        if(currentItem==1){
-            if(mMTouchListener!=null){
-                mMTouchListener.dispatchTouchEvent(event);
-            }
-        }
-        return super.onTouchEvent(event);
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
     }
 
-    MTouchListener  mMTouchListener;
-    public void setMTouchListener(MTouchListener mTouchListener){
-        this.mMTouchListener=mTouchListener;
-    }
+
 }
